68ca68eb9de9477cc62712de392add2cc7695e3c
yusunglee2074 <lys0333@gmail.com>
2020-02-28 17:11:35 +0900

    내부 로컬 DB통신 위한 모듈 작성

---
+++ src/store/Store.ts
@@ -0,0 +1,123 @@
+const Store = require('electron-store');
+
+const store = new Store();
+
+export interface INaverId {
+  id: string;
+  password: string;
+  connection?: boolean;
+}
+
+export interface INaverCafe {
+  naverId: string;
+  cafeId: string;
+  cafeName: string;
+  cafeBoardId: string;
+  cafeBoardName: string;
+}
+
+export interface ITemplate {
+  type: string;
+  title: string;
+  text: string;
+}
+
+export interface IWorking {
+  type: string;
+  title: string;
+  text: string;
+  naverId: string;
+  naverCafeId: string;
+  templateId: string;
+}
+
+export interface ILog {
+  createdAt: string;
+  title: string;
+  text: string;
+  workingId: string;
+}
+
+export const getNaverIdsOnDB = async (): Promise<Array<INaverId>> => {
+  try {
+    const ids = await store.get('naverIds');
+    if (!ids) return [];
+    return ids;
+  } catch (e) {
+    throw Error(e.message);
+  }
+};
+
+export const setNaverIdsOnDB = async (naverIds: Array<INaverId>): Promise<Array<INaverId>> => {
+  try {
+    await store.set('naverIds', naverIds);
+    return await store.get('naverIds');
+  } catch (e) {
+    throw Error(e.message);
+  }
+};
+export const getTemplatesOnDB = async (): Promise<Array<ITemplate>> => {
+  try {
+    const templates = await store.get('naverIds');
+    if (!templates) return [];
+    return templates;
+  } catch (e) {
+    throw Error(e.message);
+  }
+};
+
+export const setTemplatesOnDB = async (templates: Array<ITemplate>): Promise<Array<ITemplate>> => {
+  try {
+    await store.set('templates', templates);
+    return await store.get('templates');
+  } catch (e) {
+    throw Error(e.message);
+  }
+};
+export const getWorkingsOnDB = async (): Promise<Array<IWorking>> => {
+  try {
+    const workings = await store.get('workings');
+    if (!workings) return [];
+    return workings;
+  } catch (e) {
+    throw Error(e.message);
+  }
+};
+
+export const setWorkingsOnDB = async (workings: Array<IWorking>): Promise<Array<IWorking>> => {
+  try {
+    await store.set('workings', workings);
+    return await store.get('naverIds');
+  } catch (e) {
+    throw Error(e.message);
+  }
+};
+export const getLogsOnDB = async (): Promise<Array<ILog>> => {
+  try {
+    const logs = await store.get('logs');
+    if (!logs) return [];
+    return logs;
+  } catch (e) {
+    throw Error(e.message);
+  }
+};
+
+export const setLogsOnDB = async (logs: Array<ILog>): Promise<Array<ILog>> => {
+  try {
+    await store.set('logs', logs);
+    return await store.get('logs');
+  } catch (e) {
+    throw Error(e.message);
+  }
+};
+
+export const testStore = async () => {
+  try {
+    await store.set('test', '야호');
+    const temp = await store.get('test')
+    await store.delete('test');
+    return temp;
+  } catch (e) {
+    console.log(e);
+  }
+}
