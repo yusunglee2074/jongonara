01f9360e3b7cd4c20310eb06d0df536f8f616df4
yusunglee2074 <lys0333@gmail.com>
2020-04-02 08:50:06 +0900

    버전 표기, 네이버 아이디 관련 처리
    1. 현제 버전을 ipc통하여 가져와 표기(개발 스크립트에서는 electron버전이 표기되는 에러있음)
    2. 네이버 아이디가 연결된 로그나 작업현황에서 삭제된 네이버 아이디가 있을 시 수동으로 넣을 수 있는 로직 작성

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,16 +1,19 @@
 import * as React from 'react';
-import { Button, Col, Layout, Row, Table, Spin, message } from 'antd';
+import { Button, Col, Layout, Row, Table, Spin, message, Modal, Radio } from 'antd';
 import styled from 'styled-components';
 
-import { HOME_SCREEN_LOG_TABLE_COL, HOME_SCREEN_TEMPLATE_TABLE_COL } from '../utils/constants';
+import { APP_STRING, HOME_SCREEN_TEMPLATE_TABLE_COL } from '../utils/constants';
 import Step from '../components/Step';
 import NaverIdTable from '../components/NaverIdTable';
 import { useContext, useEffect, useState } from 'react';
 import { RootContext, RunningStatus } from '../context/AppContext';
-import { loginNaver, run } from '../ipc/renderer-IPC'
-import { setNaverIdsOnDB, setWorkingsOnDB } from '../store/Store';
+import { allStop, getVersion, listenUpdateAvailable, loginNaver, run } from '../ipc/renderer-IPC';
+import { ILog, setLogsOnDB, setNaverIdsOnDB, setWorkingsOnDB } from '../store/Store';
 import RunSettingsModal from '../components/RunSettingsModal';
 import WorkingTable from '../components/WorkingTable';
+import LogTable from '../components/LogTable';
+import { format } from 'date-fns';
+import UpdateAvailableModal from '../components/UpdateAvailableModal';
 
 const { Header, Content } = Layout;
 
@@ -29,8 +32,21 @@ const S = {
   `,
   StepTitleSpan: styled.span`
     font-size: 1.7rem;
+    margin-right: 1rem;
+  `,
+  VersionSpan: styled.span`
+    color: white;
   `
 };
+
+export enum UpdateState {
+  'newest' = '최신버전',
+  'old' = '이전버전'
+}
+export enum DownloadState {
+  'downloading' = '다운로드 중',
+  'downloaded' = '다운로드 완료'
+}
 
 const HomeScreen: React.FunctionComponent = () => {
   const {
@@ -40,15 +56,43 @@ const HomeScreen: React.FunctionComponent = () => {
     workings,
     setWorkings,
     logs,
+    setLogs,
     setting,
     runningStatus,
     setRunningStatus
   } = useContext(RootContext);
   const [loading, setLoading] = useState(true);
   const [showRunSettingsModal, setShowRunSettingsModal] = useState(false);
+  const [version, setVersion] = useState('');
+  const [updateModal, setUpdateModal] = useState({
+    showModal: false,
+    updateState: UpdateState.newest,
+    downloadState: DownloadState.downloaded
+  });
 
   useEffect(() => {
+    const getCurrentVersion = async () => {
+      setVersion(await getVersion());
+    };
     setLoading(false);
+    (async () => await getCurrentVersion())();
+
+    listenUpdateAvailable(
+      () => {
+        setUpdateModal({
+          showModal: true,
+          updateState: UpdateState.old,
+          downloadState: DownloadState.downloading
+        });
+      },
+      () => {
+        setUpdateModal({
+          showModal: true,
+          updateState: UpdateState.old,
+          downloadState: DownloadState.downloaded
+        });
+      }
+    );
   }, []);
 
   const login = async () => {
@@ -58,11 +102,8 @@ const HomeScreen: React.FunctionComponent = () => {
       // 실패한 아이디가 있을 경우
       console.log(result);
       if (result.length) {
-        //TODO: alert
-        console.log(
-          '잘못된 아이디/패스워드입니다. ' +
-            '직접 크롬이나 익스프로러 브라우저로 로그인을 진행 후 입력할 것을 권장드립니다. ' +
-            '계속 다른 패스워드로 시도될 경우 아이피가 일정기간 막힐 가능성이 있습니다.'
+        message.warn(
+          result.join(', ') + '아이디 패스워드가 일치하지 않습니다. 다시한번 확인해주세요.'
         );
         const tempNaverIds = naverIds;
         for (let i = 0; i < result.length; i++) {
@@ -85,8 +126,9 @@ const HomeScreen: React.FunctionComponent = () => {
     setLoading(false);
   };
 
-  const stop = async () => {
+  const stopRunning = async () => {
     setRunningStatus({ status: RunningStatus.Stop });
+    await allStop();
   };
 
   const start = async () => {
@@ -95,21 +137,66 @@ const HomeScreen: React.FunctionComponent = () => {
     const isWorkingExist = !!shouldRunWorkings.length;
     if (!isWorkingExist)
       return message.warn('작동할 체크된 작업이 없습니다. 작업목록에서 체크해주세요.');
+    if (shouldRunWorkings.filter(el => el.naverId === APP_STRING.deletedId).length > 0) {
+      return message.warn('삭제된 아이디가 포함되어있는 작업이 있습니다.');
+    }
     // 작업목록 validation (로그인 되어 있는지 확인)
     const shouldLoggedInIds = shouldRunWorkings.map(el => el.naverId);
     const isLoggedIn = !!naverIds.filter(
       el => shouldLoggedInIds.indexOf(el.id) > -1 && el.connection === '로그인 성공'
     ).length;
-    if (!isLoggedIn) return message.warn('작업목록에 있는 아이디를 먼저 로그인 버튼을 눌러 로그인 시켜주세요.');
+    if (!isLoggedIn)
+      return message.warn('작업목록에 있는 아이디를 먼저 로그인 버튼을 눌러 로그인 시켜주세요.');
 
     // 시작
-    await run(workings, templates, setting);
+    const logging = async (log: ILog) => {
+      setLogs([log, ...logs]);
+      await setLogsOnDB([log, ...logs]);
+    };
+    await run(workings, templates, setting, logging);
     setRunningStatus({ status: RunningStatus.Running });
+  };
+
+  const changeId = async (workingId: string) => {
+    const onChange = (e: any) => {
+      setWorkings(
+        workings.map(el => {
+          const naverId = e.target.value;
+          if (el.workingId === workingId) {
+            return { ...el, naverId };
+          } else {
+            return el;
+          }
+        })
+      );
+    };
+    Modal.info({
+      title: '변경하실 아이디를 선택해주세요.',
+      content: (
+        <Radio.Group onChange={onChange}>
+          {naverIds.length === 0 && <p>먼저 아이디를 추가해주세요.</p>}
+          {naverIds.map((el, idx) => {
+            return (
+              <Radio value={el.id} key={idx}>
+                {el.id}
+              </Radio>
+            );
+          })}
+        </Radio.Group>
+      ),
+      onOk() {},
+      okText: '확인'
+    });
+  };
+
+  const deleteLogs = async () => {
+    await setLogsOnDB([]);
+    setLogs([]);
   };
 
   return (
     <>
-      <Spin tip="로딩 중 입니다..." spinning={loading}>
+      <Spin tip="로그인 중 입니다..." spinning={loading}>
         <Header style={{ paddingLeft: 20, backgroundColor: '#003a70' }}>
           <Row>
             <Col span={16}>
@@ -118,12 +205,12 @@ const HomeScreen: React.FunctionComponent = () => {
                   시작
                 </S.HeaderButton>
               ) : (
-                <S.HeaderButton type="danger" ghost onClick={stop}>
+                <S.HeaderButton type="danger" ghost onClick={stopRunning}>
                   정지
                 </S.HeaderButton>
               )}
               <S.HeaderButton ghost onClick={() => setShowRunSettingsModal(!showRunSettingsModal)}>
-                예약 동작 설정
+                동작 설정
               </S.HeaderButton>
             </Col>
             <Col span={8}>
@@ -131,6 +218,7 @@ const HomeScreen: React.FunctionComponent = () => {
               <S.HeaderButton type="danger" ghost={true}>
                 로그아웃
               </S.HeaderButton>
+              <S.VersionSpan>버전: {version}</S.VersionSpan>
             </Col>
           </Row>
         </Header>
@@ -166,9 +254,10 @@ const HomeScreen: React.FunctionComponent = () => {
               dataSource={workings}
               title={() => <Step text="작업 목록" goto={'/setting-working'} />}
               rowSelector={{
-                selectedRowKeys: workings.filter(el => el.shouldRun).map(el => el.workingId),
+                selectedRowKeys: workings
+                  .filter(el => el.shouldRun && el.naverId !== APP_STRING.deletedId)
+                  .map(el => el.workingId),
                 onChange: async (selectedRowKeys: any) => {
-                  console.log(selectedRowKeys);
                   const newWorkings = workings.map(el => {
                     el.shouldRun = selectedRowKeys.indexOf(el.workingId) > -1;
                     return el;
@@ -177,29 +266,46 @@ const HomeScreen: React.FunctionComponent = () => {
                   await setWorkings(newWorkings);
                 }
               }}
+              extraCols={[
+                {
+                  title: '아이디 변경',
+                  render: (_: any, record: any) => {
+                    const { naverId } = record;
+                    if (naverId === APP_STRING.deletedId) {
+                      return <Button onClick={() => changeId(record.workingId)}>수정</Button>;
+                    } else {
+                      return;
+                    }
+                  }
+                }
+              ]}
             />
           </S.TableCol>
           <S.TableCol>
-            <Table
-              pagination={false}
-              rowKey={'naverId'}
-              columns={HOME_SCREEN_LOG_TABLE_COL}
+            <LogTable
               dataSource={logs}
-              bordered={true}
-              scroll={{
-                x: true,
-                y: true
-              }}
-              size={'small'}
               title={() => (
                 <>
                   <S.StepTitleSpan>작업 로그</S.StepTitleSpan>
-                  <Button icon="delete" />
+                  <Button icon="delete" onClick={deleteLogs} />
                 </>
               )}
-              locale={{
-                emptyText: '데이터가 아직 없습니다.'
-              }}
+              extraCols={[
+                {
+                  title: 'url',
+                  render: (_: any, record: any) => {
+                    const { url } = record;
+                    return <a href={url}>{url}</a>;
+                  }
+                },
+                {
+                  title: '시간',
+                  render: (_: any, record: any) => {
+                    const { createdAt } = record;
+                    return format(new Date(createdAt), 'yy/MM/dd HH:mm');
+                  }
+                }
+              ]}
             />
           </S.TableCol>
         </Content>
@@ -207,6 +313,7 @@ const HomeScreen: React.FunctionComponent = () => {
           visible={showRunSettingsModal}
           handleCancel={(_: any) => setShowRunSettingsModal(!showRunSettingsModal)}
         />
+        <UpdateAvailableModal updateModal={updateModal} setUpdateModal={setUpdateModal} />
       </Spin>
     </>
   );
